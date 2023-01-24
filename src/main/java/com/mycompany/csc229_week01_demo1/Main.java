96b392c06a65a427c79be97a1f3aa2d37e3ca850
itlabs <itlabs@WHB026_1.it.farmingdale.edu>
2023-01-24 14:34:40 -0500

    init commit

---
+++ src/main/java/com/mycompany/csc229_week01_demo1/Main.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.csc229_week01_demo1;
+
+/**
+ *
+ * @author itlabs
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    } 
+}
